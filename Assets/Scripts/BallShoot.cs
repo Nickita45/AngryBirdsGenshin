8b1050c7bf8af79931101fb897205bdbde8ec3d0
Tobito45 <tobi7799@gmail.com>
2021-12-23 12:48:52 +0100

    JumpForce

--- Assets/Scripts/BallShoot.cs
+++ Assets/Scripts/BallShoot.cs
@@ -13,16 +13,20 @@ public class BallShoot : MonoBehaviour
     [SerializeField] private bool isPressed = false;
     [SerializeField] private bool isFirst = false;
     private MusicBalls soundBall;
+    private Animation animation;
     private float randomTimeSound = 2;
     private void Start()
     {
+        animation = GetComponent<Animation>();
         rigidbody2DBullet = GetComponent<Rigidbody2D>();
         soundBall = GetComponent<MusicBalls>();
+        StartCoroutine(Jump());
     }
     private void Update()
     {
         if(isPressed)
         {
+            //animation = null;
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             if(Vector2.Distance(mousePos, rigidbody2DShoot.position) > maxDistance)
             {
@@ -63,6 +67,7 @@ public class BallShoot : MonoBehaviour
     IEnumerator LetGo()
     {
         yield return new WaitForSeconds(0.1f);
+        animation = null;
         soundBall.playflySound();
         gameObject.GetComponent<SpringJoint2D>().enabled = false;
         this.enabled = false;
@@ -77,5 +82,35 @@ public class BallShoot : MonoBehaviour
         {
             StartCoroutine(LevelManager.Instance.lostLevel());
         }
+    }
+
+    IEnumerator Jump()
+    {
+        Vector2 startposition = this.transform.position;
+
+
+        yield return new WaitForSeconds(Random.Range(2, 5));
+        while (animation != null)
+        {
+            animation.Play();
+            Vector2 targetposion = startposition + new Vector2(0, 1.2f);
+            rigidbody2DBullet.gravityScale = 0;
+            while (Vector2.Distance(transform.position, targetposion) > 0.1f)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, targetposion, 0.2f);
+                yield return new WaitForSeconds(0.1f);
+            }
+           // yield return new WaitForSeconds(0.25f);
+            while (Vector2.Distance(transform.position, startposition) > 0.1f)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, startposition, 0.2f);
+                yield return new WaitForSeconds(0.1f);
+            }
+
+            rigidbody2DBullet.gravityScale = 1;
+            yield return new WaitForSeconds(Random.Range(2, 5));
+
+        }
+
     }
 }
